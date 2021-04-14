66cb11975807c2697d495bee1171102b98bbfcc7
saraheulie <79435002+saraheulie@users.noreply.github.com>
2021-04-14 15:36:36 -0500

    somemore changes

--- forms/SleepMusic/SleepMusic.js
+++ forms/SleepMusic/SleepMusic.js
@@ -1,4 +1,34 @@
-// 1. *** use your own url copied from Postman ****
+
+//array of random search terms (made them code related)
+let searchTerms = ["factory%20functions","data%20structures", "array%20functions%20javascript", "composition%20over%20inheritance", "lambda%20functions", "streams%20java", "higher%20order%functions%javascript", "functional%20programming", "c++%20lambda%20functions", "sorting%20algorithms"];
+
+//a function to get a random search term
+let getSearchTerm = () => searchTerms[Math.floor(Math.random() * (searchTerms.length-1))];
+
+//variable for your API_KEY
+let YOUTUBE_API_KEY = "AIzaSyD-lMQaX5IUsw7Vu48XMpeDUItLFLAPaDA";
+
+//url from YouTube docs modified for my random term and API key,
+let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${getSearchTerm()}&key=${YOUTUBE_API_KEY}`;
+
+//fetch function following the aforementioned process
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.items[0].id.videoId);
+    //console.log above is to help access proper data in the JSON
+    //object
+    //set iframe source to proper URL (notice same dynamic strings 
+    //used above)
+    document.querySelector(".iframeClass").src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
+});
+
+
+btnMusic.onclick=function(){
+}
+
+
+/* // 1. *** use your own url copied from Postman ****
 let requestURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyD-lMQaX5IUsw7Vu48XMpeDUItLFLAPaDA&q=sleeping music&maxResults=1"
 
 function onXHRLoad() {
@@ -27,7 +57,7 @@ function callAPI(URL) {
     // if you DON'T need cors use this code:
     xhttp.open('GET',requestURL)
     
-    /* Headers */
+    /* Headers
     // if you need to set the returned data type, use this line of code: 
     xhttp.setRequestHeader('youtube#video', 'application/json')
     
@@ -45,7 +75,7 @@ function callAPI(URL) {
     xhttp.setRequestHeader('location','41.276900,-95.942310')
     xhttp.setRequestHeader('rankby','distance')
     xhttp.setRequestHeader('type','restaurant')
-    */
+    
 
     // make the API request
     xhttp.addEventListener('load', onXHRLoad)
@@ -53,7 +83,7 @@ function callAPI(URL) {
 }
 
 // 3. *** add a new button onclick event and put the callAPI code into it ***
+*/
 
-btnMusic.onclick=function(){
-  callAPI(requestURL)
-}
+
+
